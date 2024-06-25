b89f29f2b7b057eb2c91480e56cfb0fbaa181d29
garik.mikayelyan <garik.mikayelyan.2002@gmail.com>
2024-06-25 15:55:01 +0400

    Menu added.

---
+++ EMS/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Shared.DTOs;
+
+namespace EMS.Services;
+
+public class AuthService
+{
+	private readonly HttpClient httpClient;
+
+	public AuthService(HttpClient httpClient)
+	{
+		this.httpClient = httpClient;
+	}
+
+	public async Task<HttpResponseMessage> LoginAsync(LoginViewModel loginRequest)
+	{
+		return await httpClient.PostAsJsonAsync("/api/Auth/login", loginRequest);
+	}
+
+	public async Task<HttpResponseMessage> RegisterAsync(RegistrationViewModel registerRequest)
+	{
+		return await httpClient.PostAsJsonAsync("/api/Auth/register", registerRequest);
+	}
+}
