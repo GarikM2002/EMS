452b9d11f8046a97860dbad5750e4caf4c3d38bd
garik.mikayelyan <garik.mikayelyan.2002@gmail.com>
2024-06-27 09:16:08 +0400

    Login fixed.

---
+++ Shared/Validators/LoginViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Shared.DTOs;
+
+namespace Shared.Validators;
+
+public class LoginViewModelValidator : AbstractValidator<LoginViewModel>
+{
+	public LoginViewModelValidator()
+	{
+		RuleFor(x => x.Email)
+			.NotEmpty().WithMessage("Email is required.")
+			.EmailAddress().WithMessage("A valid email is required.");
+
+		RuleFor(x => x.Password)
+			.NotEmpty().WithMessage("Password is required.")
+			.MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+	}
+}
---
+++ Shared/Validators/RegistrationViewModelValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Shared.DTOs;
+
+namespace Shared.Validators;
+
+public class RegistrationViewModelValidator : AbstractValidator<RegistrationViewModel>
+{
+	public RegistrationViewModelValidator()
+	{
+		RuleFor(x => x.Email)
+			.NotEmpty().WithMessage("Email is required.")
+			.EmailAddress().WithMessage("A valid email is required.");
+
+		RuleFor(x => x.Password)
+			.NotEmpty().WithMessage("Password is required.")
+			.MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+
+		RuleFor(x => x.ConfirmPassword)
+			.NotEmpty().WithMessage("Confirm Password is required.")
+			.Equal(x => x.Password).WithMessage("Passwords do not match.");
+
+		RuleFor(x => x.FirstName)
+			.NotEmpty().WithMessage("First Name is required.");
+
+		RuleFor(x => x.LastName)
+			.NotEmpty().WithMessage("Last Name is required.");
+	}
+}
